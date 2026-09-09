7d8be3260e412765004cd62a1644738aa09e53b4
dklobov <d.k.lobov@gmail.com>
2026-09-09 13:50:28 +0300

    Добавляет маршруты приложения

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,47 @@
+import {Route, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import {AuthorizationStatusType} from '../../types/authorization-status';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   offersCount: number;
+  authorizationStatus: AuthorizationStatusType;
 };
 
-function App({offersCount}: AppProps): JSX.Element {
-  return <MainPage offersCount={offersCount} />;
+function App({offersCount, authorizationStatus}: AppProps): JSX.Element {
+  return (
+    <Routes>
+      <Route
+        path={AppRoute.Main}
+        element={<MainPage offersCount={offersCount} />}
+      />
+      <Route
+        path={AppRoute.Login}
+        element={<LoginPage />}
+      />
+      <Route
+        path={AppRoute.Favorites}
+        element={
+          <PrivateRoute authorizationStatus={authorizationStatus}>
+            <FavoritesPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path={AppRoute.Offer}
+        element={<OfferPage />}
+      />
+      <Route
+        path={AppRoute.NotFound}
+        element={<NotFoundPage />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
