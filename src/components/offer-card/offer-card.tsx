a7007a11c4d052ce0bf5f8d888bda76f8c856f9d
dklobov <d.k.lobov@gmail.com>
2026-09-09 18:32:55 +0300

    Делает карточку предложения динамической

--- src/components/offer-card/offer-card.tsx
+++ src/components/offer-card/offer-card.tsx
@@ -1,37 +1,97 @@
-function OfferCard(): JSX.Element {
+import {Link} from 'react-router-dom';
+import {Offer} from '../../types/offer';
+import {getOfferRoute} from '../../utils/route';
+
+const DEFAULT_CARD_CLASS_NAME = 'cities__card place-card';
+const RATING_PERCENT_MULTIPLIER = 20;
+
+type OfferCardProps = {
+  offer: Offer;
+  className?: string;
+  onOfferMouseEnter?: (offerId: string) => void;
+  onOfferMouseLeave?: () => void;
+};
+
+function getRatingWidth(rating: number): string {
+  return `${Math.round(rating) * RATING_PERCENT_MULTIPLIER}%`;
+}
+
+function getFormattedOfferType(type: Offer['type']): string {
+  return type[0].toUpperCase() + type.slice(1);
+}
+
+function OfferCard({
+  offer,
+  className = DEFAULT_CARD_CLASS_NAME,
+  onOfferMouseEnter,
+  onOfferMouseLeave,
+}: OfferCardProps): JSX.Element {
+  const {
+    id,
+    title,
+    type,
+    price,
+    isFavorite,
+    isPremium,
+    rating,
+    previewImage,
+  } = offer;
+
+  const handleCardMouseEnter = () => {
+    onOfferMouseEnter?.(id);
+  };
+
+  const handleCardMouseLeave = () => {
+    onOfferMouseLeave?.();
+  };
+
   return (
-    <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+    <article
+      className={className}
+      onMouseEnter={handleCardMouseEnter}
+      onMouseLeave={handleCardMouseLeave}
+    >
+      {isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
+
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
-        </a>
+        <Link to={getOfferRoute(id)}>
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt={title} />
+        </Link>
       </div>
+
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button
+            className={`place-card__bookmark-button ${isFavorite ? 'place-card__bookmark-button--active' : ''} button`}
+            type="button"
+          >
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
             <span className="visually-hidden">To bookmarks</span>
           </button>
         </div>
+
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{width: getRatingWidth(rating)}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
+
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <Link to={getOfferRoute(id)}>{title}</Link>
         </h2>
-        <p className="place-card__type">Apartment</p>
+
+        <p className="place-card__type">{getFormattedOfferType(type)}</p>
       </div>
     </article>
   );
