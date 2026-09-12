b99e4dd595dd2308d30010e7966f3eb20936d3b8
dklobov <d.k.lobov@gmail.com>
2026-09-12 23:50:53 +0300

    Подключает форму авторизации к серверу

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,4 +1,30 @@
+import {getAuthorizationStatus} from '../../store/selectors';
+import {useAppDispatch, useAppSelector} from '../../hooks';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import {Navigate, useNavigate} from 'react-router-dom';
+import {loginAction} from '../../store/api-actions';
+
+import {FormEvent} from 'react';
+
 function LoginPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    dispatch(loginAction({email, password}))
+      .then(() => navigate(AppRoute.Main));
+  };
+
+  if (authorizationStatus === AuthorizationStatus.Auth) {
+    return <Navigate to={AppRoute.Main} />;
+  }
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -17,14 +43,14 @@ function LoginPage(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post" onSubmit={handleSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input className="login__input form__input" type="password" name="password" placeholder="Password" required pattern="\S+" />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
