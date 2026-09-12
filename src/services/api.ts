f8133f14f848089bde96e8c2a91666776b500a93
dklobov <d.k.lobov@gmail.com>
2026-09-12 23:49:40 +0300

    Добавляет токен в запросы API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,25 @@
 import axios, {AxiosInstance} from 'axios';
+import {getToken} from './token';
+
+const TOKEN_HEADER = 'X-Token';
 
 function createApi(baseURL: string, timeout: number): AxiosInstance {
-  return axios.create({
+  const api = axios.create({
     baseURL,
     timeout,
   });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers[TOKEN_HEADER] = token;
+    }
+
+    return config;
+  });
+
+  return api;
 }
 
 export {createApi};
