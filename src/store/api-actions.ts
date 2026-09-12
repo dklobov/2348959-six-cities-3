eb14bf98805318fe08989d8c38712eef00caba03
dklobov <d.k.lobov@gmail.com>
2026-09-12 23:50:28 +0300

    Добавляет проверку авторизации и логин

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -3,8 +3,15 @@ import {AppDispatch, State} from './index';
 import {adaptOfferToClient} from '../utils/adapter';
 import {fillOffers, setOffersLoadingStatus} from './action';
 import type {ServerOffer} from '../types/offer';
+import {AuthorizationStatus} from '../const';
+import {saveToken} from '../services/token';
+import {requireAuthorization} from './action';
+import type {AuthData} from '../types/auth-data';
+import type {UserData} from '../types/user-data';
 
 const OFFERS_ROUTE = '/offers';
+
+const LOGIN_ROUTE = '/login';
 
 function fetchOffersAction() {
   return async (
@@ -25,4 +32,32 @@ function fetchOffersAction() {
   };
 }
 
-export {fetchOffersAction};
+function checkAuthAction() {
+  return async (
+    dispatch: AppDispatch,
+    _getState: () => State,
+    api: AxiosInstance
+  ): Promise<void> => {
+    try {
+      await api.get(LOGIN_ROUTE);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
+  };
+}
+
+function loginAction(authData: AuthData) {
+  return async (
+    dispatch: AppDispatch,
+    _getState: () => State,
+    api: AxiosInstance
+  ): Promise<void> => {
+    const {data} = await api.post<UserData>(LOGIN_ROUTE, authData);
+
+    saveToken(data.token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  };
+}
+
+export {checkAuthAction, fetchOffersAction, loginAction};
