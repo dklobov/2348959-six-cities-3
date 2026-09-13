f78e70d03b8f15196d12cfcd058ae79330534e4b
dklobov <d.k.lobov@gmail.com>
2026-09-13 20:02:03 +0300

    test(hooks): покрывает typed hooks Redux

---
+++ src/hooks/index.test.tsx
@@ -0,0 +1,32 @@
+import {renderHook} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import type {ReactNode} from 'react';
+import {store} from '../store';
+import {CityName} from '../const';
+import {useAppDispatch, useAppSelector} from './index';
+
+type StoreProviderProps = {
+  children: ReactNode;
+};
+
+function StoreProvider({children}: StoreProviderProps): JSX.Element {
+  return <Provider store={store}>{children}</Provider>;
+}
+
+describe('Application hooks', () => {
+  it('should return typed dispatch function', () => {
+    const {result} = renderHook(() => useAppDispatch(), {
+      wrapper: StoreProvider,
+    });
+
+    expect(typeof result.current).toBe('function');
+  });
+
+  it('should select data from store', () => {
+    const {result} = renderHook(() => useAppSelector((state) => state.app.city), {
+      wrapper: StoreProvider,
+    });
+
+    expect(result.current).toBe(CityName.Paris);
+  });
+});
