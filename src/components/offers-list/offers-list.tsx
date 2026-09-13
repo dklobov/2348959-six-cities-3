73eaf5c55eee514185bef737e88bf202879a86d2
dklobov <d.k.lobov@gmail.com>
2026-09-13 13:05:01 +0300

    Мемоизирует список предложений

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -1,5 +1,6 @@
+import {memo} from 'react';
 import OfferCard from '../offer-card/offer-card';
-import {Offer} from '../../types/offer';
+import type {Offer} from '../../types/offer';
 
 type OffersListProps = {
   offers: Offer[];
@@ -29,4 +30,6 @@ function OffersList({
   );
 }
 
-export default OffersList;
+const MemoizedOffersList = memo(OffersList);
+
+export default MemoizedOffersList;
