729ae0aef4fd91f7a2a000b7e3c7542aef30cdaf
dklobov <d.k.lobov@gmail.com>
2026-09-13 14:12:22 +0300

    Выносит шапку со счетчиком избранного

---
+++ src/components/header/header.tsx
@@ -0,0 +1,60 @@
+import {Link} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import {useAppSelector} from '../../hooks';
+import {getAuthorizationStatus, getFavoriteOffers} from '../../store/selectors';
+
+type HeaderProps = {
+  isMainPage?: boolean;
+};
+
+function Header({isMainPage = false}: HeaderProps): JSX.Element {
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  const favoriteOffers = useAppSelector(getFavoriteOffers);
+
+  const logoLinkClassName = isMainPage
+    ? 'header__logo-link header__logo-link--active'
+    : 'header__logo-link';
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <Link className={logoLinkClassName} to={AppRoute.Main}>
+              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+            </Link>
+          </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              {authorizationStatus === AuthorizationStatus.Auth ? (
+                <>
+                  <li className="header__nav-item user">
+                    <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      <span className="header__favorite-count">{favoriteOffers.length}</span>
+                    </Link>
+                  </li>
+                  <li className="header__nav-item">
+                    <a className="header__nav-link" href="#todo">
+                      <span className="header__signout">Sign out</span>
+                    </a>
+                  </li>
+                </>
+              ) : (
+                <li className="header__nav-item user">
+                  <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__login">Sign in</span>
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
